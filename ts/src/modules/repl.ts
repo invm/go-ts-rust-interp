0ecff1de261d89740adb797c2c48979c6ffd2d4c
Michael Ionov <imichaelionov@gmail.com>
2023-06-07 19:57:59 +0300

    feat: finish ts repl

--- ts/src/modules/repl.ts
+++ ts/src/modules/repl.ts
@@ -7,19 +7,14 @@ const prompt = (query: string) => new Promise((resolve) => rl.question(query, re
 
 (async () => {
   while (true) {
-    const input = await prompt(">>") as string;
+    const input = await prompt("λ ") as string;
     rl.on('pause', () => process.exit(0));
     console.log({ input });
     const l = new Lexer(input)
     for (let tok = l.nextToken(); tok.type != Tokens.ILLEGAL; tok = l.nextToken()) {
       console.log(tok);
     }
-    console.log('asd')
   }
-  rl.close();
 })();
-
-rl.on('line', (input) => console.log(`Received: ${input}`));
-rl.on('close', () => console.log(`Readline closed.`));
 
 rl.on('close', () => process.exit(0));
